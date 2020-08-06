4f5823adc17345f4b8b87b4c161558486f62df87
Jordan Lee <jordanlee80@gmail.com>
2020-08-06 11:17:52 +0800

    update

---
+++ Matrix.java
@@ -0,0 +1,30 @@
+package exercise;
+
+public class Matrix {
+    public static void print(int[][] arr) {
+
+    }
+
+    public static int[][] add(int[][] matrix1, int[][] matrix2) {
+        return null;
+    }
+
+    public static int[][] minus(int[][] matrix1, int[][] matrix2) {
+        return null;
+    }
+
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        return null;
+    }
+
+    public static int[][] transpose(int[][] matrix) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}};
+        int[][] anotherMatrix = {{1, 1, 1}, {2, 2, 2}, {0, 0, 0}};
+        int[][] thirdMatrix = {{7, 8}, {9, 10}, {11, 12}};
+
+    }
+}
