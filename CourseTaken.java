05301824648d660817508169b08aa3251072f539
Jordan Lee <jordanlee80@gmail.com>
2020-09-01 23:39:17 +0800

    update

---
+++ CourseTaken.java
@@ -0,0 +1,36 @@
+package exercise;
+
+enum Grade{A, B, C, D, F}
+/**
+ * different grade represent different score point
+ * A: 4
+ * B: 3
+ * C: 2
+ * D: 1
+ * F: 0
+ */
+
+public class CourseTaken {
+
+    private int courseCode;
+    private int credit;
+    private Grade grade;
+
+    public CourseTaken(int courseCode, int credit, Grade grade){
+        //todo add you code here
+    }
+
+    public void changeGrade(Grade grade) {
+        //todo add you code here
+    }
+
+    public int getScorePt() {
+        return 0;
+    }
+
+    public int getCredit(){
+        return 0;
+    }
+
+    //todo you may need to add any other method that you need
+}
---
+++ Student.java
@@ -0,0 +1,84 @@
+package exercise;
+
+import java.util.ArrayList;
+
+enum Gender {F, M}
+
+public class Student {
+
+    private String name;
+    private Gender gender;
+    private ArrayList<CourseTaken> courseTakenList;
+    private int age;
+    private int year;
+
+    Student(String name, Gender gender, int age, int year) {
+        //todo add you code here
+    }
+
+    /**
+     *  year will be set as current year when the user do not specify year
+     */
+    Student(String name, Gender gender, int age) {
+        //todo add your code here
+    }
+
+    /**
+     * add the course to courseTaken,
+     * if already took that course, throw IllegalStateException("cannot take the same course again")
+     */
+    public void addCourse(int courseCode, int credit, Grade grade) {
+        //todo add you code here
+    }
+
+    /**
+     * remove course from courseTaken,
+     * if no took that course before, throw IllegalStateException("never take that course before")
+     */
+    public void dropCourse(int courseCode) {
+        //todo add you code here
+    }
+
+    /**
+     *  update the grade of the course if the new grade is better than the original grade,
+     *  if never took that course, throw IllegalStateException("never take that course before")
+     */
+    public void retakeCourse(int courseCode, Grade newGrade) {
+        //todo add your code here
+    }
+
+
+    /**
+     * getter to get CGA: sum up all(course credit * score point) / total credit taken
+     */
+    public double getCGA() {
+        //todo add your code here
+        return 0;
+    }
+
+    /**
+     *  student with cga < 2 will receive warning letter
+     */
+    public boolean needReceiveWarningLetter() {
+        //todo add you code here
+        return false;
+    }
+
+    /**
+     * print the following information
+     *
+     *  Unofficial transcript
+     *  Name: xxx
+     *  Gender: xxx
+     *  Age: xxx
+     *  Course Taken:
+     *      course code: xxx    credit: xxx     grade: xxx
+     *      ...
+     *      ...
+     *   CGA in year xxx: xxx
+     */
+    public void showTranscript() {
+        //todo add your code here
+    }
+
+}
