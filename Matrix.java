b508048e111319d625995f60719e13dee019da6e
Lee Tsz Leung <jordanlee80@gmail.com>
2020-09-10 12:45:53 +0800

    add javadoc

--- Matrix.java
+++ Matrix.java
@@ -1,6 +1,11 @@
 package exercise;
 
 public class Matrix {
+    
+    /**
+     * print the matrix
+     * @param arr the 2d int array representation of matrix
+     */
     public static void print(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -48,8 +53,6 @@ public class Matrix {
         int[][] matrix2 = {{1, 3, 5}, {2, 4, 6}};
         int[][] anotherMatrix = {{1, 1, 1}, {2, 2, 2}, {0, 0, 0}};
         int[][] thirdMatrix = {{7, 8}, {9, 10}, {11, 12}};
-
-//        print(matrix);
 
         print(add(matrix, matrix2));
         print(minus(matrix, matrix2));
